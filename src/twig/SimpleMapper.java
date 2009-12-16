7a60db6d1d0b8d9b1fc87abb3e985ed22f5c3303
Ben Page [w] <benpage26@gmail.com>
2009-12-16 01:33:52 +0000

    touched SimpleMapper.java
    I am testing the branches

---
+++ src/twig/SimpleMapper.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package twig;
+
+/**
+ *
+ * @author ben
+ */
+public class SimpleMapper {
+
+}
